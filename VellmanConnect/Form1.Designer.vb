727c9511ee3dafd0209e74401357b4fc83fae26d
Martin Weston <martinweston90@gmail.com>
2019-09-18 22:11:26 +0100

    Add project files.

---
+++ VellmanConnect/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(57, 47)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(108, 28)
+        Me.btnSend.TabIndex = 0
+        Me.btnSend.Text = "Send Text"
+        Me.btnSend.UseVisualStyleBackColor = True
+
+        Me.Display = New Velleman.Kits.K8101(Me.components)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSend As Button
+    Friend WithEvents Display As Velleman.Kits.K8101
+End Class
